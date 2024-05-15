4ace253047b5a30bfac03b7ac066608291c947a8
Everbluee <147749884+Everbluee@users.noreply.github.com>
2024-05-15 14:17:37 +0200

    Added docstrings to utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,18 @@
 def add(a: int, b: int) -> int:
+    """Function for adding numbers"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """Function for substracting numbers"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    """Function for multiplying numbers"""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
+    """Function for dividing numbers"""
     return a / b
